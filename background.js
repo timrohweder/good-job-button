3db5e904abb8fa03310cea2ae4a58f92c0286879
Tim Rohweder <trohweder85@gmail.com>
2020-07-29 21:24:23 -0700

    Increase audio volume, add icon hover text

--- background.js
+++ background.js
@@ -1,9 +1,12 @@
 // icons from Muhammad Haq at freeicons.io
 // sound from user metrostock99 at freesound.org
 
+console.log("hey there");
+
 const audio = new Audio("sounds/oh-yeah.wav");
 
 chrome.browserAction.onClicked.addListener(() => {
+  audio.volume = 0.8;
   audio.play();
 
   chrome.browserAction.setIcon({
